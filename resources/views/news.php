ebab04135b6267d4df898b887fc73e25b0a6c2ef
VT <vera.tishelovich@mail.ru>
2020-07-30 12:10:24 +0300

    ДЗ к уроку 1

---
+++ resources/views/news.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Новости</title>
+
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        .news {
+
+            width: 80%;
+            margin-left: 10%;
+
+
+
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <div class="content">
+        <div class="title m-b-md">
+
+            Новости
+        </div>
+
+        <div class="links">
+
+
+            <b>Хочешь вернуться на главную </b><br><br><a href="/public"><button>Нажми сюда</button></a><br><br>
+            <b>Хочешь узнать больше о проекте </b><br><br><a href="/public/project"><button>Нажми сюда</button></a><br><br>
+
+        </div>
+
+        <br><br>
+        <div class="news">
+            <b>Новость 1</b><br>
+            <span>Quos explicabo ea culpa sit facilis illo delectus, labore officia consectetur dolores eum natus, optio, temporibus rerum deserunt voluptatibus dolorum odit modi corrupti. Dicta ab, debitis iste exercitationem provident hic.</span>
+            <span>Harum quod, quasi eos voluptate neque. Obcaecati veniam fugit adipisci, consequatur optio nobis, voluptates laudantium mollitia omnis odio dolor quo quia repellendus harum, amet veritatis impedit! Vero esse magni dolorum!</span>
+        </div>
+        <br><br>
+        <div class="news">
+            <b>Новость 2</b><br>
+            <span>Quos explicabo ea culpa sit facilis illo delectus, labore officia consectetur dolores eum natus, optio, temporibus rerum deserunt voluptatibus dolorum odit modi corrupti. Dicta ab, debitis iste exercitationem provident hic.</span>
+            <span>Harum quod, quasi eos voluptate neque. Obcaecati veniam fugit adipisci, consequatur optio nobis, voluptates laudantium mollitia omnis odio dolor quo quia repellendus harum, amet veritatis impedit! Vero esse magni dolorum!</span>
+        </div><br><br>
+
+
+        <div class="news">
+            <b>Новость 3</b><br>
+            <span>Quos explicabo ea culpa sit facilis illo delectus, labore officia consectetur dolores eum natus, optio, temporibus rerum deserunt voluptatibus dolorum odit modi corrupti. Dicta ab, debitis iste exercitationem provident hic.</span>
+            <span>Harum quod, quasi eos voluptate neque. Obcaecati veniam fugit adipisci, consequatur optio nobis, voluptates laudantium mollitia omnis odio dolor quo quia repellendus harum, amet veritatis impedit! Vero esse magni dolorum!</span>
+        </div>
+
+
+
+
+    </div>
+
+</body>
+
+</html>
---
+++ resources/views/project.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Информация о проекте</title>
+
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        .info {
+
+
+            width: 80%;
+            margin-left: 10%;
+            text-align: justify;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <div class="content">
+        <div class="title m-b-md">
+
+            Информация о нашем проекте
+        </div>
+
+        <div class="links">
+
+
+            <b>Хочешь вернуться на главную </b><br><br><a href="/public"><button>Нажми сюда</button></a><br><br>
+            <b>Хочешь почитать новости <b><br><br> <a href="/public/news"><button>Нажми сюда</button></a>
+
+        </div>
+
+        <br><br>
+        <div class="info">
+            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam est amet laboriosam vero. Repellendus nihil ex saepe! Quibusdam, nulla veritatis laborum nihil provident earum sint! Quis, consectetur, voluptatibus. Facilis, excepturi!</span>
+            <span>Saepe quae natus quo odit impedit consequatur quos minus neque laboriosam aut, placeat modi debitis atque tempore architecto inventore eaque! Distinctio tempora, atque veritatis laudantium! Consequatur dolorem earum, eveniet provident.</span>
+            <span>Neque expedita tempore culpa fugiat doloribus reprehenderit molestias consectetur adipisci id libero. Repellat ullam iure molestiae earum ducimus facere, voluptates libero odit, fugit necessitatibus ea at velit soluta totam? Quisquam.</span>
+            <span>Doloribus vero praesentium nisi qui quam corrupti. At nulla nihil quibusdam dolorum eos id ad, voluptatibus voluptate tempore natus quidem libero, quos autem officia hic facere iste distinctio unde perspiciatis.</span>
+            <span>>Quos explicabo ea culpa sit facilis illo delectus, labore officia consectetur dolores eum natus, optio, temporibus rerum deserunt voluptatibus dolorum odit modi corrupti. Dicta ab, debitis iste exercitationem provident hic.</span>
+            <span>Harum quod, quasi eos voluptate neque. Obcaecati veniam fugit adipisci, consequatur optio nobis, voluptates laudantium mollitia omnis odio dolor quo quia repellendus harum, amet veritatis impedit! Vero esse magni dolorum!</span>
+            <span>Eveniet, quasi suscipit exercitationem, cupiditate ut, libero tempore accusamus quis qui facilis pariatur rem provident ex soluta necessitatibus, odio inventore nesciunt. Odio magni, veritatis cum enim natus nemo error sint!</span>
+            <span>Ad id qui, aut, asperiores tempore praesentium sapiente voluptates, eos cupiditate porro nobis. Voluptates dolores, animi minima placeat, officia corporis cum culpa porro, impedit eaque architecto obcaecati dignissimos aliquam eum!</span>
+            <span>Ad sit eos voluptatum. Ex veritatis ea placeat inventore, qui impedit possimus quos laboriosam sit, minima aut esse debitis doloribus officia iusto cumque error maiores pariatur consequuntur. Sint sit, enim.</span>
+            <span>Porro dolore laudantium facilis quisquam ipsum nisi rem molestias nemo commodi veniam consequatur repellendus atque reprehenderit tempora sequi accusantium totam earum asperiores iure magnam aliquam, laborum ducimus reiciendis culpa. Eveniet.</span>
+            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam est amet laboriosam vero. Repellendus nihil ex saepe! Quibusdam, nulla veritatis laborum nihil provident earum sint! Quis, consectetur, voluptatibus. Facilis, excepturi!</span>
+            <span>Saepe quae natus quo odit impedit consequatur quos minus neque laboriosam aut, placeat modi debitis atque tempore architecto inventore eaque! Distinctio tempora, atque veritatis laudantium! Consequatur dolorem earum, eveniet provident.</span>
+            <span>Neque expedita tempore culpa fugiat doloribus reprehenderit molestias consectetur adipisci id libero. Repellat ullam iure molestiae earum ducimus facere, voluptates libero odit, fugit necessitatibus ea at velit soluta totam? Quisquam.</span>
+            <span>Doloribus vero praesentium nisi qui quam corrupti. At nulla nihil quibusdam dolorum eos id ad, voluptatibus voluptate tempore natus quidem libero, quos autem officia hic facere iste distinctio unde perspiciatis.</span>
+            <span>Quos explicabo ea culpa sit facilis illo delectus, labore officia consectetur dolores eum natus, optio, temporibus rerum deserunt voluptatibus dolorum odit modi corrupti. Dicta ab, debitis iste exercitationem provident hic.</span>
+            <span>Harum quod, quasi eos voluptate neque. Obcaecati veniam fugit adipisci, consequatur optio nobis, voluptates laudantium mollitia omnis odio dolor quo quia repellendus harum, amet veritatis impedit! Vero esse magni dolorum!</span>
+            <span>Eveniet, quasi suscipit exercitationem, cupiditate ut, libero tempore accusamus quis qui facilis pariatur rem provident ex soluta necessitatibus, odio inventore nesciunt. Odio magni, veritatis cum enim natus nemo error sint!</span>
+            <span>Ad id qui, aut, asperiores tempore praesentium sapiente voluptates, eos cupiditate porro nobis. Voluptates dolores, animi minima placeat, officia corporis cum culpa porro, impedit eaque architecto obcaecati dignissimos aliquam eum!</span>
+            <span>Ad sit eos voluptatum. Ex veritatis ea placeat inventore, qui impedit possimus quos laboriosam sit, minima aut esse debitis doloribus officia iusto cumque error maiores pariatur consequuntur. Sint sit, enim.</span>
+            <span>Porro dolore laudantium facilis quisquam ipsum nisi rem molestias nemo commodi veniam consequatur repellendus atque reprehenderit tempora sequi accusantium totam earum asperiores iure magnam aliquam, laborum ducimus reiciendis culpa. Eveniet.</span>
+            <span>Quos explicabo ea culpa sit facilis illo delectus, labore officia consectetur dolores eum natus, optio, temporibus rerum deserunt voluptatibus dolorum odit modi corrupti. Dicta ab, debitis iste exercitationem provident hic.</span>
+            <span>Harum quod, quasi eos voluptate neque. Obcaecati veniam fugit adipisci, consequatur optio nobis, voluptates laudantium mollitia omnis odio dolor quo quia repellendus harum, amet veritatis impedit! Vero esse magni dolorum!</span>
+            <span>Eveniet, quasi suscipit exercitationem, cupiditate ut, libero tempore accusamus quis qui facilis pariatur rem provident ex soluta necessitatibus, odio inventore nesciunt. Odio magni, veritatis cum enim natus nemo error sint!</span>
+            <span>Ad id qui, aut, asperiores tempore praesentium sapiente voluptates, eos cupiditate porro nobis. Voluptates dolores, animi minima placeat, officia corporis cum culpa porro, impedit eaque architecto obcaecati dignissimos aliquam eum!</span>
+            <span>Ad sit eos voluptatum. Ex veritatis ea placeat inventore, qui impedit possimus quos laboriosam sit, minima aut esse debitis doloribus officia iusto cumque error maiores pariatur consequuntur. Sint sit, enim.</span>
+            <span>Porro dolore laudantium facilis quisquam ipsum nisi rem molestias nemo commodi veniam consequatur repellendus atque reprehenderit .Sint sit, enim.</span>
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
